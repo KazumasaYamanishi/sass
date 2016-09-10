632c40aa9d50d88baa1f799465957e94e42502f6
KazumasaYamanishi <addas.baboon@gmail.com>
2016-09-10 12:32:05 +0900

    gulp で自動処理

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+
+
+
+var gulp 		= require('gulp');
+var sass 		= require('gulp-sass');
+var plumber 	= require('gulp-plumber');
+
+
+
+// Sassコンパイルタスク
+gulp.task('sass', function(){
+	gulp.src('./sass/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(gulp.dest('./css/'));
+});
+
+
+
+// watchタスク(**/*.scss変更時に実行するタスク)
+gulp.task('sass-watch', ['sass'], function(){
+	var watcher = gulp.watch('./sass/**/*.scss', ['sass']);
+	watcher.on('change', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	});
+});
+
+
+
+// gulpのデフォルト動作としてsass-watchを実行
+gulp.task('default', ['sass-watch']);
